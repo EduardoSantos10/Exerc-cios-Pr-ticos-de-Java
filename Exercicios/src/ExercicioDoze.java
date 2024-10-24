8b85dd7fa230f1ff6f1e5b14c7166f983a71c73a
Eduardo <eduardo25082002@gmail.com>
2024-10-24 20:56:29 -0300

    Quarto Commit

--- Exercicios/src/ExercicioDoze.java
+++ Exercicios/src/ExercicioDoze.java
@@ -5,5 +5,19 @@ public class ExercicioDoze {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Informe um número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Informe um número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Informe um número: ");
+        int num3 = scanner.nextInt();
+
+        double media = ((num1 + num2 + num3) / 3);
+
+        System.out.println(media);
+
+        scanner.close();
     }
 }
---
+++ Exercicios/src/ExercicioTreze.java
@@ -0,0 +1,16 @@
+
+public class ExercicioTreze {
+    public static void main(String[] args) {
+        double largura = 5.6;
+        double altura = 8.5;
+
+        int num1 = 2;
+
+        double res1 = largura * altura;
+
+        double per1 = num1 * (largura + altura);
+
+        System.out.println(res1);
+        System.out.println(per1);
+    }
+}
